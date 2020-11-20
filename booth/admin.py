07ff55b1cedbba9b2ddc0d51ca94eada99d12f85
Comma <48902622+sinpyo@users.noreply.github.com>
2020-11-21 02:21:23 +0900

    부스 분류 관리자페이지

--- booth/admin.py
+++ booth/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from booth.models import BoothCategory
+
 
 # Register your models here.
+@admin.register(BoothCategory)
+class BoothCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    list_per_page = 10
+    #    list_filter = ['']
+    search_fields = ['name']
