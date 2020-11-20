a732a13f4ccd83f74ef438205cde8e530955458e
bell2lee <paperlee@kookmin.ac.kr>
2020-11-20 22:20:22 +0900

    hotfix : Time model 제거

--- booth/models.py
+++ booth/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 import re
 from guest.models import AuthUser
-from guest.models import TimeStampedModel
 
 class Region(models.Model):
     host = models.ForeignKey(AuthUser, verbose_name="주최 집단", on_delete=models.CASCADE, null=False)
@@ -45,7 +44,7 @@ class Booth(models.Model):
     # 첨부파일 추가
 
     
-class Review(models.Model, TimeStampedModel):
+class Review(models.Model):
     stamp_id = models.CharField(max_length=10, null=False, unique=True, verbose_name="스탬프 uuid")
     rating = models.PositiveIntegerField(verbose_name="평점")
     review = models.TextField(verbose_name="평가")
@@ -56,11 +55,12 @@ class Like(models.Model):
     code = models.ForeignKey(Booth, verbose_name="부스 코드", on_delete=models.CASCADE, null=False)
 
     
-class Notice(models.Model, TimeStampedModel):
+class Notice(models.Model):
     name = models.CharField(max_length=100, null=False, verbose_name="공지 제목")
     description = models.TextField(null = False, blank=False, verbose_name="공지 내용")
     onclick_target = models.ForeignKey(Booth, on_delete=models.SET_NULL, null=True)
     phone = PhoneNumberField(blank=True, null=True, verbose_name="학생회 번호")
+    created_at = models.DateTimeField(auto_now=True, null=False, verbose_name="작성 시간")
 
     
 class HashTag(models.Model):
@@ -70,6 +70,6 @@ class HashTag(models.Model):
     def __str__(self):
         return self.name
 
-class Stamp(models.Model, TimeStampedModel):
+class Stamp(models.Model):
     owner = models.ForeignKey(AuthUser, on_delete=models.SET_NULL, null=True)
     code = models.ForeignKey(Booth, verbose_name="부스 코드", on_delete=models.CASCADE, null=False)
